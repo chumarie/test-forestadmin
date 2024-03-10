f1246523d216c60223c53cbde677eef1df3ba017
Marie Chu <marie.chu@tekos.net>
2024-03-10 23:06:13 +0700

    update users schema

--- models/users.js
+++ models/users.js
@@ -2,15 +2,27 @@ const Mongoose = require("mongoose");
 
 const schema = new Mongoose.Schema(
   {
-    email: String,
-    lastname: String,
-    firsname: String,
+    username: {
+      type: String,
+      required: [true, "Please provide a username"],
+      unique: true,
+    },
+    email: {
+      type: String,
+      required: [true, "Please provide a email"],
+      unique: true,
+    },
+    password: {
+      type: String,
+      required: [true, "Please provide a password"],
+    },
+    forgotPasswordToken: String,
+    forgotPasswordTokenExpiry: Date,
   },
   {
     timestamps: false,
   }
 );
-
 module.exports = {
   collectionName: "users",
   modelName: "users",
